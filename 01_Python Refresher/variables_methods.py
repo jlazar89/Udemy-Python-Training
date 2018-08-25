9e3fb857900d22673939885bff83f9437b0466ae
Jeffy Lazar <jlazar89@gmail.com>
2018-08-25 10:11:58 +0530

    Variables and methods
    Python declaring variables and methods

---
+++ 01_Python Refresher/variables_methods.py
@@ -0,0 +1,29 @@
+a = 5
+b = 10
+
+my_variable = 16
+string_variable ="Hello world"
+
+
+# Methods
+def print_method(arg):
+    print(arg)
+
+print_method("Jeffy Lazar")
+
+
+def multiple(num1, num2):
+    return num1 * num2
+
+print_method(multiple(2,5))
+
+# complete the method byu returning 42
+def return_42():
+    return 42
+
+# created a method that takes two arguments and return multiplied result
+def my_method(num1,num2):
+    return num1 * num2
+
+print_method(return_42())
+print_method(my_method(8,9))
